930c43d91bda5277163ce0f98115453343e201db
Ulysses Carlos <ulycarlos@gmail.com>
2020-10-07 00:31:40 -0400

    Finished Chapter13_06.cc

---
+++ Chapter13_07.cc
@@ -0,0 +1,66 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 10/07/2020 at 12:20 AM
+ * 
+ * Chapter13_07.cc
+ * Make an RGB color chart (e.g., search the web for “RGB color chart”).
+ * -----------------------------------------------------------------------------
+ */
+
+#include "GUI/Simple_window.h"
+using namespace Graph_lib;
+using Simple_Window = Simple_window;
+
+class Box : public Rectangle {
+public:
+    Box(Point p, int w, int h, std::string t, int fs)
+	: Rectangle(p, w, h), width{w}, height{h}, font_size{fs},
+	  text{t} {
+	if (font_size < 1)
+	    error("Font size is negative.");
+	// Now check if the string will fit the box:
+	if ((width - t.size() / 2) + t.size() > width)
+	    error("Text will not fit the width of the box.");
+
+    }
+
+    Box(Point p, int w, int h, std::string t):
+	Rectangle(p, w, h), width{w}, height{h}, text{t} { }
+
+    void draw_lines() const;
+    void set_label(const string &str){ text = str; }
+    string label() const { return text; }
+
+    void set_font_size(int size) { font_size = size; }
+    int get_font_size() { return font_size; }
+    
+private:
+    int width;
+    int height;
+    int font_size{14};
+    std::string text;
+    Graph_lib::Font fnt{fl_font()};
+};
+
+void Box::draw_lines() const {
+    Rectangle::draw_lines(); // Call Rectangle's draw_lines
+    
+    // Now draw a text box, centered vertically
+    int ofnt = fl_font();
+    int osz = fl_size();
+    fl_font(fnt.as_int(), font_size);
+
+    fl_draw(text.c_str(), point(0).x + ((width - text.size()) / 8),
+	    point(0).y + height / 2);
+    fl_font(ofnt, osz);
+
+}
+
+int main(void){
+    Simple_Window win{Point{0, 0}, 1600, 900, "Chapter13_07"};
+    Vector_ref<Box> vrec;
+    
+    
+    win.wait_for_button();
+
+}
